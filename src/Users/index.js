92cb20579d434458d21db7b467c9ea045037cecc
kodruck <jonathan.salazar@ucrso.info>
2023-07-05 19:12:24 -0600

    add recovery password and changepassword

--- src/Users/index.js
+++ src/Users/index.js
@@ -6,8 +6,11 @@ const { UsersController } = require('./controller');
 module.exports.UsersAPI = (app) => {
   router
   .get('/', UsersController.getUsers)//Se utiliza para concatenar
+  .get('/recuperarContrasenna/:id', UsersController.recoveryPass)
+  
   .post('/signup',UsersController.registerUser)
   .post('/login',UsersController.loginUser)
+  .post('/changePassword',UsersController.changePassword)
   
   app.use('/api/users', router)
 }
